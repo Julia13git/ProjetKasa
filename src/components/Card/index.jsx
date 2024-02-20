0fe34b0c02d7f4e2b989526e34c54a3ab21e03dd
Yuliia Hots <goculiya@gmail.com>
2024-02-20 17:49:46 +0100

    Affichage les images dans les cards

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,19 +1,17 @@
-import PropTypes from 'prop-types'
 import '../../styles/scss/card.scss'
 import { Link } from 'react-router-dom'
+//import { housingList } from '../../datas/housingList'
 
-function Card({id,  title }) {
+
+function Card({id, title, cover }) {
   return (
       <Link to={''} key={id} className="onecard" >
+        <img src={cover} alt={title} />
         <div className='one-card-content'>
-          <h2>{title} </h2>
+          <h2>{title}</h2>
         </div>
       </Link>
   )
-}
-Card.propTypes = {
-  id: PropTypes.string,
-  title: PropTypes.string.isRequired
 }
 
 export default Card
--- src/components/Cardlist/index.jsx
+++ src/components/Cardlist/index.jsx
@@ -1,14 +1,23 @@
 import { housingList } from '../../datas/housingList'
-import Card from '../../components/Card'
+//import Card from '../../components/Card'
+import { Link } from 'react-router-dom'
+import '../../styles/scss/card.scss'
 
 function Cardlist() {
     return (
       <div className='card-list'>
         {housingList.map((housing) => (
-          <Card
-            key={housing.id}
-            title={housing.title}
-          />
+          // <Card
+          //   key={housing.id}
+          //   img={housing.cover}
+          //   title={housing.title}
+          // />
+          <Link to={''} key={housing.id} className="onecard" >
+        <img src={housing.cover} alt={housing.title} className='card-img'/>
+        <div className='one-card-content'>
+          <h2>{housing.title}</h2>
+        </div>
+      </Link>
         ))}
       </div>
     )
